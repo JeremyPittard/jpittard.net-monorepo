681dbde8aa7f4a494423f6972790ebf43ae2b7ce
Jeremy Pittard <jeremy@jpittard.net>
2023-10-24 15:53:56 +0800

    remove github mode for now

--- apps/thoughts/keystatic.config.ts
+++ apps/thoughts/keystatic.config.ts
@@ -2,11 +2,7 @@ import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "github",
-    repo: {
-      owner: "JeremyPittard",
-      name: "jpittard.net-monorepo",
-    },
+    kind: "local",
   },
   collections: {
     posts: collection({
