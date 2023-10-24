1a8ded6fdbbba4f76c7434cfbbc33bda616c1e92
Jeremy Pittard <jeremy@jpittard.net>
2023-10-24 15:50:16 +0800

    thoughts github storage

--- apps/thoughts/keystatic.config.ts
+++ apps/thoughts/keystatic.config.ts
@@ -1,25 +1,29 @@
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: "github",
+    repo: {
+      owner: "JeremyPittard",
+      name: "jpittard.net-monorepo",
+    },
   },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
+        title: fields.slug({ name: { label: "Title" } }),
         content: fields.document({
-          label: 'Content',
+          label: "Content",
           formatting: true,
           dividers: true,
           links: true,
           images: {
-            directory: 'src/assets/images/posts',
-            publicPath: '../../assets/images/posts/',
+            directory: "src/assets/images/posts",
+            publicPath: "../../assets/images/posts/",
           },
         }),
       },
