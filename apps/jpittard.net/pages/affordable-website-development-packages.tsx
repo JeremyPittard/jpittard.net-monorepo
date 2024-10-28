60ed227ea4858f84e2867307aa307524d56eb433
Jeremy Pittard <jeremy@jpittard.net>
2024-10-28 08:13:30 +0800

    added new page for new offer to promote on the fbs

---
+++ apps/jpittard.net/pages/affordable-website-development-packages.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+import { motion } from "framer-motion";
+
+const WebsiteDevelopmentOfferPage = () => {
+
+    return(
+        <Layout>
+            <motion.main>
+                <h1>some thing</h1>
+            </motion.main>
+        </Layout>
+    )
+}
